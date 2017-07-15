21732cefe2c973d4222f786f581ceb9f7670a664
Thomas Allen <tom@telega.org>
2017-07-15 15:34:21 +0100

    fooditem

---
+++ foodItem.js
@@ -0,0 +1,22 @@
+const{
+	itemId: t.Integer,
+	steps:t.Array(
+		{	
+		locationName: t.String,
+		geoloc:
+			{
+				lat: t.Number,
+			 	long: t.Number,
+			 }
+		dateIn: t.Date,
+		dateOut: t.Date
+		facilityName: t.String,
+		facilityAccredation: t.String,
+		facilityChemicals: t.Array(t.String),
+		facilityAllergens: t.Array(t.String),
+		conditionIn: t.String,
+		conditionOut: t.String,
+		}
+	)
+	componentItems:t.Array(t.Integer),
+}
