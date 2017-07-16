d85b0bb70217827c6036d681634716ac24b235da
ThomasAnkcorn <thomasankcorn@gmail.com>
2017-07-16 03:07:44 +0100

    get-all function promise

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ mongo.connect(driver, (db) => {
         function getPassports(id) {
             return getPassport(id)
                 .then(item => {
+                    console.log(item)
                     return Promise.all(item.links.forEach(function (subItem) {
                         console.log('subItem');
                         return getPassports(subItem)
